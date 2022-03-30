c1f814a94baf620054fe6741578167582b5b4f17
Priyanshu Anany <84434172+priyanshuanany@users.noreply.github.com>
2022-03-30 11:23:32 +0530

    Update userController.js

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -5,13 +5,49 @@ const User = require("../model/userModel");
 
 const router = express.Router();
 
-router.post("/", async(req, res) => {
-    try{
-        const user = await User.create(req.body);
-        return res.status(201).send(user);
-    }catch(err) {
-        return res.status(500).send({message: err.message});
-    }
+router.post(
+    "/",
+    body("firstName")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("first Name cannot be empty"),
+    
+    body("email")
+    .isEmail()
+    .normalizeEmail()
+    .custom(async(value) => {
+        const user = await User.findOne({email: value});
+
+        if(user) {
+            throw new Error("Email is already taken");
+        }
+        return true;
+    }),
+    body("pincode")
+    .isNumeric()
+    .isLength({min: 6})
+    .isPostalCode(),
+
+    body("age")
+    .isNumeric()
+    .not().isEmpty()
+    .withMessage("Age cannot be empty")
+    .custom((value) => {
+        if(value < 1 || value > 100){
+            throw new Error("Incorrect age provided");
+        }
+        return true;
+    }),
+
+
+    async(req, res) => {
+        try{
+            const user = await User.create(req.body);
+            return res.status(201).send(user);
+        }catch(err) {
+            return res.status(500).send({message: err.message});
+        }
 });
 
 module.exports = router;
